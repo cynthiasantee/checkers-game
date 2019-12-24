b6698aefee12ef9994acd9a2cd3d1bdcd81a9111
Cynthia Santee <santeecynthia@gmail.com>
2019-12-23 18:03:04 -0800

    crate board and pieces

--- src/State/slices.ts
+++ src/State/slices.ts
@@ -1,9 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialBoard = [
+    [{squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}],
+    [{squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}],
+    [{squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'white', isDouble: false}}],
+    [{squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}],
+    [{squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: null}],
+    [{squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}],
+    [{squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}],
+    [{squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}, {squareColor: 'black', piece: {id: 1, color: 'black', isDouble: false}}, {squareColor: 'white', piece: null}],
+]
+
 export const adder = createSlice({
-    name: 'name',
-    initialState: 0 as number,
+    name: 'board',
+    initialState: 0,
     reducers: {
         increment: state => state + 1
 }})
 
+type Color = 'white' | 'black';
+
+interface Piece {
+    id: number,
+    color: Color,
+    isDouble: boolean,
+}
+
+interface BoardSquare {
+    squareColor: Color,
+    piece: Piece
+}
