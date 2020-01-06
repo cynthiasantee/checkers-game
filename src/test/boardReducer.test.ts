7228f3f624eae926bf7d13e1e6600dc32167c10e
Cynthia Santee <santeecynthia@gmail.com>
2020-01-05 16:07:22 -0800

    Add comment to boardReducer

--- src/test/boardReducer.test.ts
+++ src/test/boardReducer.test.ts
@@ -3,7 +3,7 @@ import { board } from '../State/slices'
 describe('board reducer', () => {
   it('should move the piece', () => {
         const newBoard = board.reducer(
-            undefined, 
+            undefined, // the first time the reducer is called, it is unndefined
             board.actions.movePiece({ pieceId: 9,location: [3, 2]})
         );
         expect(newBoard[2][1].piece).toEqual(null);
