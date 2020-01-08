06562ddf278a8e4b5a96be72ac61e05655f08675
Cynthia Santee <santeecynthia@gmail.com>
2020-01-08 14:20:57 -0800

    Modify getValidMoves function

--- src/test/getValidMoves.test.ts
+++ src/test/getValidMoves.test.ts
@@ -1,8 +1,40 @@
-import { initialBoard } from '../State/slices'
 import { getValidMoves } from '../util/getValidMoves'
+import { movePiece } from '../util/movePiece'
+import { printBoard } from '../util/printBoard';
+
 
 describe('getValidMoves function', () => {
-    it('should return an array of valid moves', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  });
+
+    it('should return 2 take moves', () => {
+      const { initialBoard } = require('../State/slices') 
       expect(getValidMoves([5, 4], initialBoard)).toStrictEqual([[4,3], [4, 5]]);
-    })
+    });
+
+    it('should return 1 take and 1 move', () => {
+      const { initialBoard } = require('../State/slices')
+      movePiece([2, 3], [3, 4], initialBoard);
+      movePiece([3, 4], [4, 5], initialBoard);
+      expect(getValidMoves([5, 4], initialBoard)).toStrictEqual([[4, 3], [3, 6]]);
+    });
+
+    it('should return 0 takes and 1 move', () => {
+      const { initialBoard } = require('../State/slices')
+      expect(getValidMoves([5, 0], initialBoard)).toStrictEqual([[4, 1]]);
+    });
+
+    it('should return 1 take and 0 move', () => {
+      const { initialBoard } = require('../State/slices')
+      movePiece([5, 6], [4, 7], initialBoard);
+      movePiece([4, 7], [3, 6], initialBoard);
+      expect(getValidMoves([2, 7], initialBoard)).toStrictEqual([[4, 5]]);
+    });
+
+    it('should return 0 moves', () => {
+      const { initialBoard } = require('../State/slices')
+      expect(getValidMoves([6, 1], initialBoard)).toStrictEqual([]);
+
+    });
 })
