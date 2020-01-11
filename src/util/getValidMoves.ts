9e15c52247d9ac16ac459aa1b7dd143cee03ca23
Cynthia Santee <santeecynthia@gmail.com>
2020-01-10 19:04:13 -0800

    Add isDouble logic to movePiece function

--- src/util/getValidMoves.ts
+++ src/util/getValidMoves.ts
@@ -6,17 +6,18 @@ export const getValidMoves = (curr: Location, board: BoardSquare[][]): Location[
     const [i, j] = curr;
     const piece = board[i][j].piece;
     const validMoves: Location[] = []
+
+    const whiteFL: Location = [i+1,j-1]
+    const whiteTL: Location = [i+2,j-2]
+    const whiteFR: Location = [i+1,j+1]
+    const whiteTR: Location = [i+2,j+2]
+    const blackFL: Location = [i-1,j-1]
+    const blackTL: Location = [i-2,j-2]
+    const blackFR: Location = [i-1,j+1]
+    const blackTR: Location = [i-2,j+2]
     
-    if (piece) {
+    if (piece?.isDouble === false) {
         const pieceColor = piece.color
-        const whiteFL: Location = [i+1,j-1]
-        const whiteTL: Location = [i+2,j-2]
-        const whiteFR: Location = [i+1,j+1]
-        const whiteTR: Location = [i+2,j+2]
-        const blackFL: Location = [i-1,j-1]
-        const blackTL: Location = [i-2,j-2]
-        const blackFR: Location = [i-1,j+1]
-        const blackTR: Location = [i-2,j+2]
         
         if (pieceColor === 'white') {
             if (board[whiteFL[0]][whiteFL[1]]?.piece === null) {
@@ -43,7 +44,31 @@ export const getValidMoves = (curr: Location, board: BoardSquare[][]): Location[
                 validMoves.push(blackTR)
             }
         }
-    } 
+    } else if (piece?.isDouble === true) {
+        if (board[whiteFL[0]][whiteFL[1]]?.piece === null) {
+            validMoves.push(whiteFL);
+        } else if (board[blackTL[0]][blackTL[1]]?.piece === null && !board[blackFL[0]][blackFL[1]].piece?.color) {
+            validMoves.push(blackTL)
+        }
+
+        if (board[whiteFR[0]][whiteFR[1]]?.piece === null) {
+            validMoves.push(whiteFR);
+        } else if (board[whiteTR[0]][whiteTR[1]]?.piece === null && !board[whiteFR[0]][whiteFR[1]].piece?.color) {
+            validMoves.push(whiteTR)
+        }
+
+        if (board[blackFL[0]][blackFL[1]]?.piece === null) {
+            validMoves.push(blackFL);
+        } else if (board[blackTL[0]][blackTL[1]]?.piece === null && !board[blackFL[0]][blackFL[1]].piece?.color) {
+            validMoves.push(blackTL)
+        }
+
+        if (board[blackFR[0]][blackFR[1]]?.piece === null) {
+            validMoves.push(blackFR);
+        } else if (board[blackTR[0]][blackTR[1]]?.piece === null && !board[blackFR[0]][blackFR[1]].piece?.color) {
+            validMoves.push(blackTR)
+        }
+    }
 
     return validMoves.filter(isOnBoard)
 }
