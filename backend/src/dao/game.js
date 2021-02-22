cdca9004e2fb56e3e6798b6c1ca915fb7bfa3434
Cynthia Santee <santeecynthia@gmail.com>
2021-02-22 09:23:27 -0800

    Add dao layers

---
+++ backend/src/dao/game.js
@@ -0,0 +1,88 @@
+import {pgClient} from "../pool.js";
+
+const getGames = async (_, res) => {
+    try {
+      const games = await pgClient.query('SELECT * FROM game');
+      res.status(200).send(games.rows);
+    } catch (err) {
+      console.log(err)
+        res
+          .status(500)
+          .send(`Encountered an internal error when fetching games`);
+    }
+  };
+  
+  const getGameById = async (req, res) => {
+    const id = parseInt(req.params.id)
+  
+    try {
+      const game = await pgClient.query(
+        `SELECT * FROM game WHERE id = $1`,
+        [id])
+  
+      res.status(200).send(game.rows);
+    } catch (err) {
+      console.log(err)
+        res
+          .status(500)
+          .send(`Encountered an internal error when fetching game`);
+    }
+  };
+  
+  const createGame = async (req, res) => {
+    const { player_one_id } = req.body;
+      try {
+        await pgClient.query(
+          `INSERT INTO game (player_one_id) VALUES ($1)`,
+          [player_one_id])
+    
+          res.status(201).send(`Game created`);
+      } catch (err) {
+        console.log(err);
+        res
+        .status(500)
+        .send('Encountered an internal error when creating game');
+      }
+  };
+  
+  const setSecondPlayer = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { player_two_id } = req.body;
+  
+    try {
+      await pgClient.query(
+        "UPDATE game SET player_two_id = $1 WHERE id = $2",
+        [player_two_id, id])
+        res.status(200).send(`Game updated`);
+    } catch (err) {
+      console.log(err)
+      res
+      .status(500)
+      .send('Encountered an internal error when updating game');
+    }
+  };
+
+  const setWinner = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { winner_id } = req.body;
+  
+    try {
+      await pgClient.query(
+        "UPDATE game SET winner_id = $1 WHERE id = $2",
+        [winner_id, id])
+        res.status(200).send(`Game updated`);
+    } catch (err) {
+      console.log(err)
+      res
+      .status(500)
+      .send('Encountered an internal error when updating game');
+    }
+  };
+  
+  export const gameDAO = {
+    getGames,
+    getGameById,
+    createGame,
+    setSecondPlayer,
+    setWinner,
+  };
--- backend/src/dao/player.js
+++ backend/src/dao/player.js
@@ -1,4 +1,4 @@
-import {pgClient} from "../index.js";
+import {pgClient} from "../pool.js";
 
 const getPlayers = async (_, res) => {
   try {
@@ -13,58 +13,105 @@ const getPlayers = async (_, res) => {
 };
 
 const getPlayerById = async (req, res) => {
-//   const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id)
 
-//   const player = await pgClient.query('SELECT * FROM player WHERE id = $1', [id])
-//   .catch(e => {
-//     res
-//       .status(500)
-//       .send(`Encountered an internal error when fetching item with ID ${id}`);
-//   });
-// res.status(200).send(player.rows)
-};
-
-const createPlayer = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const player = await pgClient.query(
+      `SELECT * FROM player WHERE id = $1`,
+      [id])
 
-    await pgClient.query(
-      `INSERT INTO player (email, password) VALUES ($1, $2)`,
-      [email, password])
-
-      res.status(201).send(`Player created`);
+    res.status(200).send(player.rows);
   } catch (err) {
-    console.log(`this is my error:`, err);
-    res
-    .status(500)
-    .send('Encountered an internal error when creating an item');
+    console.log(err)
+      res
+        .status(500)
+        .send(`Encountered an internal error when fetching player`);
   }
 };
 
-const updatePassword = async (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const { email, password } = req.body;
+const createPlayer = async (req, res) => {
+  const { email, password } = req.body;
+    try {
+      await pgClient.query(
+        `INSERT INTO player (email, password) VALUES ($1, $2)`,
+        [email, password])
+  
+        res.status(201).send(`Player created`);
+    } catch (err) {
+      console.log(err);
+      res
+      .status(500)
+      .send('This email account has already been registered');
+    }
 
-//   await pgClient.query(
-//     "UPDATE player SET email = $1, password = $2 WHERE id = $3",
-//     [email, password, id])
-//     .catch(e => {
-//       res
-//         .status(500)
-//         .send('Encountered an internal error when updating an item');
-//     });
-// res.status(200).send(`Player updated with ID: ${id}`);
+};
+
+const updatePassword = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { email, password } = req.body;
+
+  try {
+    await pgClient.query(
+      "UPDATE player SET email = $1, password = $2 WHERE id = $3",
+      [email, password, id])
+      res.status(200).send(`Player updated with ID: ${id}`);
+  } catch (err) {
+    console.log(err)
+    res
+    .status(500)
+    .send('Encountered an internal error when updating an item');
+  }
 };
 
 const deletePlayer = async (req, res) => {
-  // const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-  // await pgClient.query("DELETE FROM player WHERE id = $1", [id], (error, results) => {
-  //   if (error) {
-  //     throw error;
-  //   }
-  //   res.status(200).send(`Player deleted with ID: ${id}`);
-  // });
+  try {
+    await pgClient.query(
+      "DELETE FROM player WHERE id = $1",
+      [id])
+
+      res.status(200).send(`Player deleted with ID: ${id}`);
+  } catch (err) {
+    console.log(err)
+    res
+    .status(500)
+    .send('Encountered an internal error when deleting player');
+  }
+};
+
+const getPlayerWins = async (req, res) => {
+  const id = parseInt(req.params.id)
+
+  try {
+    const player = await pgClient.query(
+      `SELECT COUNT(winner_id) FROM game WHERE winner_id = $1`,
+      [id])
+
+    res.status(200).send(player.rows);
+  } catch (err) {
+    console.log(err)
+      res
+        .status(500)
+        .send(`Encountered an internal error when fetching wins`);
+  }
+};
+
+const getPlayerTotalGames = async (req, res) => {
+  const id = parseInt(req.params.id)
+
+  try {
+    const player = await pgClient.query(
+      `SELECT COUNT(id) FROM game WHERE player_one_id = $1 OR player_two_id = $1`,
+      [id])
+
+    res.status(200).send(player.rows);
+  } catch (err) {
+    console.log(err)
+      res
+        .status(500)
+        .send(`Encountered an internal error when fetching wins`);
+  }
 };
 
 export const playerDAO = {
@@ -73,4 +120,6 @@ export const playerDAO = {
   createPlayer,
   updatePassword,
   deletePlayer,
+  getPlayerWins,
+  getPlayerTotalGames
 };
