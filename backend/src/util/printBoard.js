48da29636b516ffc9daa0ba3efdc4e702e8794dc
Cynthia Santee <santeecynthia@gmail.com>
2021-03-09 14:44:08 -0800

    Add double piece move

--- backend/src/util/printBoard.js
+++ backend/src/util/printBoard.js
@@ -7,9 +7,19 @@ export const printBoard = (board) => {
             if (square.piece === null) {
               return "O";
             } else if (square.piece.color === "black") {
-              return "B";
+                if (square.piece.isDouble) {
+                    return "X"
+                } else {
+                    return "B";
+                }
             } else if (square.piece.color === "white") {
-              return "W";
+                if (square.piece.isDouble) {
+                    return "Y"
+                } else {
+                    return "W";
+                }
+            } else {
+                return "X"
             }
           })
           .join("");
